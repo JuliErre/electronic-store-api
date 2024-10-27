80958f45e7155c52e934514d40f78845db97ce44
JuliErre <julian.erreguerena@gmail.com>
2024-10-27 16:08:56 -0300

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ItemsModule } from './items/items.module';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
@@ -22,8 +21,6 @@ import { UsersModule } from './users/users.module';
         uri: configService.get<string>('MONGO_URI'),
       }),
     }),
-
-    ItemsModule,
     ProductsModule,
     UserModule,
     UsersModule,
